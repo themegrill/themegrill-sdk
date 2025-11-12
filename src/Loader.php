bdcd3b56256ce3126054d0dabb22fe8563b0becc
asheshmagar <ashesh.ido@gmail.com>
2025-11-12 17:30:29 +0545

    chore: version update

--- src/Loader.php
+++ src/Loader.php
@@ -32,7 +32,7 @@ final class Loader {
 	 *
 	 * @var string $version The class version.
 	 */
-	private static $version = '1.0.0';
+	private static $version = '1.0.1';
 	/**
 	 * Holds registered products.
 	 *
